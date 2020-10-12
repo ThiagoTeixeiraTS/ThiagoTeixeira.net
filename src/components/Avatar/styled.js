4a1ad169beed210d69bf84608b17d1e6c2219157
Thiago Teixeira <thiagoemecal@gmail.com>
2020-10-12 14:17:53 -0300

    StyledComponent -  por component

---
+++ src/components/Avatar/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Img from "gatsby-image";
+
+
+export const AvatarWrapper = styled(Img) `
+    border-radius : 50%;
+    height: 3.75rem;
+    margim: auto;
+    width: 3.75rem;
+    
+`
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Avatar from "../Avatar";
+import * as S from "./styled";
+
 
 const Profile = () => {
     const {
@@ -21,15 +23,18 @@ const Profile = () => {
 `)
 
     return (
-        <div className="Profile-wrapper">
-            <Avatar />
-            <h1>{title}</h1>
-            <h2>{position}</h2>
-            <p>
-                `Teste ` {description}
-            </p>
-        </div   >
+        <S.ProfileWrapper>
 
+            <S.ProfileLink>
+                <Avatar />
+                <S.ProfileAuthor>
+                    {title}
+                    <S.ProfilePosition>{position}</S.ProfilePosition>
+                </S.ProfileAuthor>
+            </S.ProfileLink>
+
+            <S.ProfileDescription>{description}</S.ProfileDescription>
+            </S.ProfileWrapper>
 
     )
 }
