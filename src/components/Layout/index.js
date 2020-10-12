328e2496845294dbe912431dfac707f5079a7be1
Thiago Teixeira <thiagoemecal@gmail.com>
2020-10-12 19:11:32 -0300

    Create MenuBar Component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Sidebar from "../Sidebar";
+import React from "react"
+import PropTypes from "prop-types"
+import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
-import * as S from "./styled";
-import GlobalStyles from "../../styles/global";
+import * as S from "./styled"
+import GlobalStyles from "../../styles/global"
 
 const Layout = ({ children }) => {
   return (
-        <S.LayoutWrapper>
-          <GlobalStyles />
-          <Sidebar/>
-          <S.LayoutMain>{children}</S.LayoutMain>
-        </S.LayoutWrapper>
+    <S.LayoutWrapper>
+      <GlobalStyles />
+      <Sidebar />
+      <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
+    </S.LayoutWrapper>
   )
 }
 
@@ -19,4 +21,4 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout;
+export default Layout
