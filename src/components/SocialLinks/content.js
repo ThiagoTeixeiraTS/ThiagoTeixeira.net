886ce6f4237d65c426cb97050bf4969f3bb5c1f6
Thiago Teixeira <thiagoemecal@gmail.com>
2020-10-12 15:21:09 -0300

    Social Links

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+        label: "Github", 
+        url: "https://github.com/ThiagoTeixeiraTS",
+    }, 
+    {
+        label: "Twitter", 
+        url: "https://twitter.com/ThiagoEmecal",
+    }, 
+    {
+        label: "Instagram", 
+        url: "https://www.instagram.com/thiagotexera/",
+    }
+]
+
+export default links;
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,18 @@
+import { GitHub } from '@styled-icons/boxicons-logos/Github';
+import { Twitter } from '@styled-icons/boxicons-logos/Twitter';
+import { Instagram } from '@styled-icons/boxicons-logos/InstagramAlt';
+import { Xbox } from '@styled-icons/fa-brands/Xbox';
+import { Site } from '@styled-icons/boxicons-logos/DevTo';
+import { LinkdIn} from '@styled-icons/boxicons-logos/LinkedinSquare';
+import { Email } from '@styled-icons/boxicons-regular/At';
+const Icons = {
+    GitHub, 
+    Twitter,
+    Instagram, 
+    Xbox, 
+    Site,
+    LinkdIn, 
+    Email
+}
+
+export default Icons;
