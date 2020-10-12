9f30a464d9aaabb27be9d570394b19369999fa4f
Thiago Teixeira <thiagoemecal@gmail.com>
2020-10-12 11:05:36 -0300

    Gatsby Image, Criando o Avatar do meu site

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-
+import Avatar from "../Avatar";
 
 const Profile = () => {
     const {
@@ -22,6 +22,7 @@ const Profile = () => {
 
     return (
         <div className="Profile-wrapper">
+            <Avatar />
             <h1>{title}</h1>
             <h2>{position}</h2>
             <p>
