3d99e226d076193b6f47e491db2ea48e16705061
Thiago Teixeira <thiagoemecal@gmail.com>
2020-10-12 15:39:15 -0300

    SideBar Cont

--- src/components/SocialLinks/icons.js
+++ src/components/SocialLinks/icons.js
@@ -1,18 +1,12 @@
-import { GitHub } from '@styled-icons/boxicons-logos/Github';
+import { Github } from '@styled-icons/boxicons-logos/Github';
 import { Twitter } from '@styled-icons/boxicons-logos/Twitter';
 import { Instagram } from '@styled-icons/boxicons-logos/InstagramAlt';
-import { Xbox } from '@styled-icons/fa-brands/Xbox';
-import { Site } from '@styled-icons/boxicons-logos/DevTo';
 import { LinkdIn} from '@styled-icons/boxicons-logos/LinkedinSquare';
-import { Email } from '@styled-icons/boxicons-regular/At';
 const Icons = {
-    GitHub, 
+    Github, 
     Twitter,
     Instagram, 
-    Xbox, 
-    Site,
-    LinkdIn, 
-    Email
+    LinkdIn
 }
 
 export default Icons;
